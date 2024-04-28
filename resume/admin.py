24aeba226b1b9588277932d5c7ddc596e98b9ad2
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-04-28 16:26:13 -0400

    feat(models): Create Skill and Experience models with corresponding ModelAdmin and apply corrections to PersonalData

--- resume/admin.py
+++ resume/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import PersonalData
+from .models import PersonalData, Skill, Experience
 from solo.admin import SingletonModelAdmin
 
 # Register your models here.    
 admin.site.register(PersonalData, SingletonModelAdmin)
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('skill', 'main_skill')
+    list_filter = ('main_skill',)
+    search_fields = ('skill',)
+
+admin.site.register(Skill, SkillAdmin)
+
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('job_title', 'company', 'start_date', 'finish_date', 'current_job')
+    list_filter = ('current_job',)
+    search_fields = ('job_title', 'company')
+
+admin.site.register(Experience, ExperienceAdmin)
--- resume/models.py
+++ resume/models.py
@@ -7,9 +7,9 @@ from solo.models import SingletonModel
 class PersonalData(SingletonModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    birthday = models.DateField(null=True)
+    birthday = models.DateField(blank=True, null=True)
     nacionality = models.CharField(max_length=100, null=True)
-    
+    about_me = models.TextField(blank=True)
     class Meta:
         verbose_name = "My personal data"
 
@@ -20,3 +20,22 @@ class PersonalData(SingletonModel):
         today = date.today()
         age = today.year - self.birthday.year - ((today.month, today.day) < (self.birthday.month, self.birthday.day))
         return age
+
+class Skill(models.Model):
+    skill = models.CharField(max_length=100)
+    main_skill = models.BooleanField(default=False)
+    logo = models.ImageField(upload_to='media/skills/', blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.skill}"
+
+class Experience(models.Model):
+    job_title = models.CharField(max_length=150)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField(blank=True, null=True)
+    finish_date = models.DateField(blank=True, null=True)
+    current_job = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.job_title} {self.company}"
