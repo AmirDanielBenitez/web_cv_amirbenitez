cab6335c23e216beffe577ff87ae52fea245f174
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-04-28 17:37:43 -0400

    feat(models): Create Project model

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description')
+    list_filter = ('platform',)
+    search_fields = ('name',)
+    
+admin.site.register(Project, ProjectAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Project (models.Model):
+    WEB = 'web'
+    APP = 'app'
+    PLATFORM_CHOICES = [
+        (WEB, 'Web'),
+        (APP, 'Mobile'),
+    ]
+    
+    name = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to='media/portfolio/', blank=True, null=True)
+    platform = models.CharField(max_length=10, choices=PLATFORM_CHOICES)
+    
+    def __str__(self):
+        return self.name
+    
