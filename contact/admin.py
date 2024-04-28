206c2345c9facdf3704cb263737c1e90a8f4d212
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-04-28 17:10:08 -0400

    feat(models): Create ContactData model as singleton

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ContactData
+from solo.admin import SingletonModelAdmin
 
 # Register your models here.
+admin.site.register(ContactData, SingletonModelAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from solo.models import SingletonModel
 
 # Create your models here.
+class ContactData(SingletonModel):
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=13)
+    linkedin = models.URLField(max_length=200)
+    whatsapp_url = models.URLField(max_length=200) 
+    
+    def __str__(self):
+        return f"Contact Data"
+    
+    def formatted_phone_number(self):
+        digits_only = ''.join(filter(str.isdigit, self.phone_number))
+        if len(digits_only) > 10:
+            return f"(+{digits_only[:3]}) {digits_only[3:6]}-{digits_only[6:]}"
+        else:
+            return f"{digits_only[:4]}-{digits_only[4:]}"
