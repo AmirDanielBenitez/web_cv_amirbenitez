3642d6befa3f22ec2d327a0aab42d598510973a7
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-04-29 21:27:51 -0400

    feat(view): Add initial class view and templates for HomePage

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.views.generic.base import TemplateView
+from resume.models import PersonalData
 # Create your views here.
+class HomePage(TemplateView):
+    template_name='core/home_page.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        personalData = PersonalData.get_solo()
+        context["name"] = f'{personalData.first_name} {personalData.last_name}'
+        return context
+    
