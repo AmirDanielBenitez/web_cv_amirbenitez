063a8bea864d2f0693311c44bb8a1ebf18ca0108
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-05-03 21:49:38 -0400

    feat: Add design for 'About Me' section and contact data functional

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from resume.models import PersonalData
+from contact.models import ContactData
 # Create your views here.
 class HomePage(TemplateView):
     template_name='core/home_page.html'
@@ -9,7 +10,10 @@ class HomePage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         personalData = PersonalData.get_solo()
+        contactData = ContactData.get_solo()
         context["name"] = f'{personalData.first_name} {personalData.last_name}'
         context["short_description"] = personalData.short_description
+        context["about_me"] = personalData.about_me
+        context["contact_data"] = contactData
         return context
     
