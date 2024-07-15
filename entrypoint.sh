68a767c192021765da061e36618af37953af1fe0
Amir Benitez <amir.daniel.b.d@gmail.com>
2024-07-14 21:23:05 -0400

    feat: Add Railway configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-echo "Waiting for DB..."
-while ! nc -z db 5432; do
-  sleep 1
-done
-echo "DB ready"
+self.stdout.write('Waiting for database...')
+db_conn = False
+
+while not db_conn:
+    try:
+        c = connection.cursor()
+        c.execute('SELECT 1')
+        db_conn = True
+    except OperationalError:
+        self.stdout.write('Database unavailable, waiting 1 second...')
+        sleep(1)
+
+self.stdout.write(self.style.SUCCESS('Database available!'))
 
 echo 'Running collecstatic...'
 echo "Recolectando archivos estáticos..."
 python manage.py collectstatic --no-input
 
 echo 'Applying migrations...'
-echo "Aplicando migraciones..."
 python manage.py migrate
 
 echo 'Running server...'
